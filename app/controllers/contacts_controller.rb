728b6d84cf25daba0045a45f4177b8248bbd423e
yexyle24 <yejielw@hotmail.com>
2016-06-21 00:22:45 +0000

    contactos 2

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,19 @@ class ContactsController < ApplicationController
     def new
         @contact = Contact.new
     end
-    
-    def create
+    def create 
+       @contact = Contact.new(contact_params)
+       
+        if @contact.save 
+            flash[:success] = "mensaje enviao"
+        else 
+            flash[:error] = 'ocurrio un error'
+           
+        end
     end
+    private 
+        def contact_params
+           params.require(:contact).permit(:name, :email, :comments) 
+        end
+
 end
